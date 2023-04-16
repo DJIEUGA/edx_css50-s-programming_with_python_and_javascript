fb6c84e45beb94cc209c2c830741ce05ff1d8edc
Djieuga Djoubi <djieugadjoubirylenathan@gmail.com>
2023-04-16 15:56:52 +0100

    python unit testing, test-driven development and assert

---
+++ testing/testDrivenDev/test.sh
@@ -0,0 +1,5 @@
+python -c "from test import test_prime; test_prime(1, False)"
+python -c "from test import test_prime; test_prime(2, True)"
+python -c "from test import test_prime; test_prime(8, False)"
+python -c "from test import test_prime; test_prime(11, True)"
+python -c "from test import test_prime; test_prime(25, False)"
